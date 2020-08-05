ebadb754777b64821f5c9c58822ba6681eae8e9b
= <=>
2020-08-05 22:11:25 +0300

    Linking Added on Drawer

--- components/DrawerContent.js
+++ components/DrawerContent.js
@@ -1,25 +1,41 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Linking } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
+
 
 const DrawerContent = () => {
     return (
         <View>
-            <TouchableOpacity style={styles.touchableButton}>
-                <Text style={styles.touchableButtonText}>Link 1</Text>
+            <TouchableOpacity 
+            style={styles.touchableButton}>
+                <Text 
+                style={styles.touchableButtonText}>Link 1</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.touchableButton}>
-                <Text style={styles.touchableButtonText}>Link 2</Text>
+
+            <TouchableOpacity 
+            style={styles.touchableButton}>
+                <Text 
+                style={styles.touchableButtonText}>Link 2</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.touchableButton}>
-                <Text style={styles.touchableButtonText}>Link 3</Text>
+
+            <TouchableOpacity 
+            style={styles.touchableButton}>
+                <Text 
+                style={styles.touchableButtonText}>Link 3</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.touchableButton}>
+
+            <TouchableOpacity 
+            style={styles.touchableButton}
+            onPress={() => {Linking.openURL('https://expo.io');}}>
                 <Text style={styles.touchableButtonText}>Rate us</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.touchableButton}>
-                <Text style={styles.touchableButtonText}>Share</Text>
+
+            <TouchableOpacity 
+            style={styles.touchableButton}>
+                <Text 
+                style={styles.touchableButtonText}>Share</Text>
             </TouchableOpacity>
+
         </View>
     )
 }

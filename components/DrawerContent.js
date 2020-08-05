bc824fab16c43d1c834fd1789e676ee366d61776
= <=>
2020-08-05 23:55:34 +0300

    Drawer Icons Added

--- components/DrawerContent.js
+++ components/DrawerContent.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, View, Linking, BackHandler } from 'react-native'
+import { StyleSheet, Text, View, Linking, BackHandler, Image } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 
 
@@ -27,11 +27,20 @@ const DrawerContent = () => {
             <TouchableOpacity 
             style={styles.touchableButton}
             onPress={() => {Linking.openURL('https://expo.io');}}>
+                <Image 
+                source={require("../assets/star.png")}
+                fadeDuration={0}
+                style={styles.iconStyle}/>
                 <Text style={styles.touchableButtonText}>Rate us</Text>
             </TouchableOpacity>
 
             <TouchableOpacity 
             style={styles.touchableButton}>
+                <Image 
+                source={require("../assets/share.png")}
+                fadeDuration={0}
+                style={styles.iconStyle}/>
+                <Text style={styles.touchableButtonText}></Text>
                 <Text 
                 style={styles.touchableButtonText}>Share</Text>
             </TouchableOpacity>
@@ -39,6 +48,10 @@ const DrawerContent = () => {
             <TouchableOpacity 
             style={styles.touchableButton}
             onPress={() => {BackHandler.exitApp();}}>
+                <Image 
+                source={require("../assets/quit.png")}
+                fadeDuration={0}
+                style={styles.iconStyle}/>
                 <Text 
                 style={styles.touchableButtonText}>Quit</Text>
             </TouchableOpacity>
@@ -54,9 +67,16 @@ const styles = StyleSheet.create({
         alignItems:"center",
         backgroundColor: "#690375",
         paddingVertical:30,
+        flexDirection:"row",
+        paddingStart:20
     },
     touchableButtonText:{
         fontSize:24,
         color:"#E7D7C1"
+    },
+    iconStyle:{
+        width:20,
+        height:20,
+        marginRight:5
     }
 })
